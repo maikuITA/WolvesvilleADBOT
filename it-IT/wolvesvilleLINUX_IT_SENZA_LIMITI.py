06a20c94a996ec0b9abebc5c67de0c0751de42df
Giulio <86263020+maikuITA@users.noreply.github.com>
2025-01-08 15:45:19 +0100

    Linux script update
    Added time approximation and button click approximation

--- it-IT/wolvesvilleLINUX_IT_SENZA_LIMITI.py
+++ it-IT/wolvesvilleLINUX_IT_SENZA_LIMITI.py
@@ -25,34 +25,42 @@ sleep = 0.05
 
 # Displays
 display = xlib.XOpenDisplay(None)
-root = xlib.XDefaultRootWindow(display)
+root = xlib.XDefaultRootwaitindowait(display)
 
 def setPos(x, y):
-    xlib.XWarpPointer(display,None,root,0,0,0,0,x,y)
+    xlib.XwaitarpPointer(display,None,root,0,0,0,0,x,y)
     
-def countdown():
+def countdowaitn():
     startup = 5
     print("### SCRIPT REALIZZATO DA maiku ")
-    print("### Wolvesville AD BOT LINUX VERSION")
+    print("### waitolvesville AD BOT LINUX VERSION")
     print("Avvio dello script tra", startup, "secondi...")
     time.sleep(startup)
     print()    
     
 def findButton(x, y):
+    tries = 1
+    max_tries = 10
     while(True):
         try:
             if gui.pixelMatchesColor(x, y, (229, 229, 231)):
                 #print("DEBUG: WATCH VIDEO!!", x, ",", y)
                 return x, y
             elif y < 400:
+                #print("Exception")
                 raise Exception
             else:
-                #print("DEBUG: WATCH VIDEO non trovato a ", x, ",", y)
+                #print("DEBUG: waitATCH VIDEO non trovato a ", x, ",", y)
                 y = y - 2
         except:
-            print("[!] Non ho trovato il bottone, aspetto 10 secondi e riprovo...")
-            y = backup
-            time.sleep(10)
+            if tries < max_tries+1:
+                print(f"[{tries}/{max_tries}] Bottone non trovato")
+                y = backup
+                time.sleep(5)
+                tries += 1
+            else:
+                print("[%] Tentativi esauriti, fermo lo script")
+                return
 
 m = PyMouse()
 def leftClick(x, y):
@@ -65,32 +73,43 @@ def approx():
     ms = numpy.random.randint(133, 931)
     return ms/1000
     
+def offset(x, y):
+    print("Before offset", x, y)
+    y -= numpy.random.randint(7, 19)
+    if numpy.random.randint(1, 2) % 2 == 0:
+        x += numpy.random.randint(7, 74)
+    else:
+        x -= numpy.random.randint(7, 74)  
+    print("After offset", x, y)
+    return x, y 
+    
 def run():
+    # Adding offset to mouse click
     bx, by = findButton(x, y)
+    bx, by = offset(bx, by)
     leftClick(bx, by) # WATCH VIDEO
-    w = numpy.random.randint(37, 49)
-    w += approx()
-    print("[1] WATCH BUTTON... OK {attesa:",w,"secondi}")
-    time.sleep(w)
+    wait = numpy.random.randint(37, 49) + approx()
+    print("[1] waitATCH BUTTON... OK {attesa:",wait,"secondi}")
+    time.sleep(wait)
     
     rightClick(bx, by) # CHIUSURA AD 
-    w = numpy.random.randint(5, 14)
-    w += approx()
-    print("[2] AD... OK {attesa:",w,"secondi}")
-    time.sleep(w)
+    wait = numpy.random.randint(5, 14) + approx()
+    print("[2] AD... OK {attesa:",wait,"secondi}")
+    time.sleep(wait)
     
     bx, by = findButton(x, y)
+    bx, by = offset(bx, by)
     leftClick(bx, by) # SPIN 
     print("[3] SPIN... OK")
-    w = numpy.random.randint(15, 20)
-    w += approx()
-    print("[4] Attesa finale:",w,"secondi")
-    time.sleep(w) # attesa finale della ricompensa + scarto
+    wait = numpy.random.randint(15, 20)
+    wait += approx()
+    print("[4] Attesa finale:",wait,"secondi")
+    time.sleep(wait) # attesa finale della ricompensa + scarto
     print()
 
 ##########################################################################################
 
-countdown()
+countdowaitn()
 ripetizioni = 1
 while(True):
     print("[?] RIPETIZIONE NUMERO:", ripetizioni)
